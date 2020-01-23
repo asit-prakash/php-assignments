4a24f02d82e2d5a52c2911bf429a9cf85e48b794
asit-prakash <58478104+asit-prakash@users.noreply.github.com>
2020-01-23 23:32:20 +0530

    updated url path feature

--- login/assignment1.php
+++ login/assignment1.php
@@ -82,11 +82,40 @@
             if($_SERVER["REQUEST_METHOD"] == "POST")
             {
                 if(isset($_POST['logout']))
-                    {
+                {
                     session_destroy();
                     header("Location:index.php");
-                    } 
+                } 
+			}
+			if(isset($_GET["q"]))
+        {
+            $get_path = $_GET["q"];
+            if($get_path == '1')
+            {
+                header("location:assignment1.php");
+                exit;
             }
+            if($get_path == '2')
+            {
+                header("location:assignment2.php");
+                exit;
+            }
+            if($get_path == '3')
+            {
+                header("location:assignment3.php");
+                exit;
+            }
+            if($get_path == '4')
+            {
+                header("location:assignment4.php");
+                exit;
+            }
+            if($get_path == '5')
+            {
+                header("location:assignment5.php");
+                exit;
+            }
+        }
         ?>
 
 		<h2>Welcome
--- login/assignment3.php
+++ login/assignment3.php
@@ -98,7 +98,36 @@
                     session_destroy();
                     header("Location:index.php");
                     } 
+			}
+			if(isset($_GET["q"]))
+        {
+            $get_path = $_GET["q"];
+            if($get_path == '1')
+            {
+                header("location:assignment1.php");
+                exit;
             }
+            if($get_path == '2')
+            {
+                header("location:assignment2.php");
+                exit;
+            }
+            if($get_path == '3')
+            {
+                header("location:assignment3.php");
+                exit;
+            }
+            if($get_path == '4')
+            {
+                header("location:assignment4.php");
+                exit;
+            }
+            if($get_path == '5')
+            {
+                header("location:assignment5.php");
+                exit;
+            }
+        }
         ?>
 
 		<h2>Welcome
